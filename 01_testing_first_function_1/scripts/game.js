b0c2f20ab6d82d1fa04c77d63cbd59e3749e580d
al-ell <132070392+al-ell@users.noreply.github.com>
2023-09-09 17:25:24 +0000

    initial commit

--- 01_testing_first_function_1/scripts/game.js
+++ 01_testing_first_function_1/scripts/game.js
@@ -1,5 +1,7 @@
 let game = {
     currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "buttons2", "button3", "button4"],
     score: 0,
 };
 
--- 01_testing_first_function_1/scripts/tests/game.test.js
+++ 01_testing_first_function_1/scripts/tests/game.test.js
@@ -2,6 +2,8 @@
  * @jest-environment jsdom
  */
 
+const { default: test } = require("node:test");
+
 
 
 
@@ -21,6 +23,15 @@ describe("game object contains correct keys", () => {
     });
     test("currentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "buttons2", "button3", "button4"]);
     });
 });
 
